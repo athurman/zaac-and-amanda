ad24a69e9d8d1fac7db9b6965b2768992f557f63
athurman <athurman215@gmail.com>
2016-01-20 08:43:38 -0600

    add general app style file

---
+++ app/components/GeneralStyles/GeneralStyles.js
@@ -0,0 +1,30 @@
+const style = {}
+
+// colors
+style.colors = {
+  black: '#37373C',
+  teal: '#87D0C7',
+  white: '#FFFEF5',
+  whiteHover: '#E3E2DA',
+}
+
+// site
+style.site = {
+  backgroundColor: style.colors.black,
+  width: '100%',
+  color: style.colors.white,
+}
+
+// fonts
+style.font = {
+  mainFont: '\'Raleway\', sans-serif',
+}
+
+// pageHeader
+style.pageHeader = {
+  fontSize: 40,
+  textAlign: 'center',
+}
+
+
+export default style
